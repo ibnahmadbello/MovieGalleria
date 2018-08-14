d8225624073716c0bba6ee43e33a1b91ac6aab42
regent <justforlearningcode@gmail.com>
2018-08-14 10:26:13 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/regent/moviegalleria/MovieGalleriaActivity.java
@@ -0,0 +1,13 @@
+package com.example.regent.moviegalleria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieGalleriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_movie_galleria);
+    }
+}
